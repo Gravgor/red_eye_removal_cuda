25cf3e3b61b892638a2f9e2a861588be8424bf07
Marceli Borowczak <marcelborowczak@outlook.com>
2024-03-23 14:48:56 +0100

    refactor: new code of removing red eyes

--- red-eye-removal/red-eye-removal/kernel.cu
+++ red-eye-removal/red-eye-removal/kernel.cu
@@ -7,95 +7,108 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
+#define CUDA_CHECK(call) \
+    do { \
+        cudaError_t error = call; \
+        if (error != cudaSuccess) { \
+            fprintf(stderr, "CUDA error: %s, line %d\n", cudaGetErrorString(error), __LINE__); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0)
 
-// This project is a red eye remover. It takes an image and removes the red eye from it with use of CUDA.
+#define STBI_CHECK(call) \
+    do { \
+        if (!(call)) { \
+            fprintf(stderr, "STB image error: %s\n", stbi_failure_reason()); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0)
 
-// The image is loaded and the red eye is detected by checking the red channel of the image. If the red channel is above a certain threshold, the pixel is marked as red eye.
 
-// The red eye is then replaced with the average color of the surrounding pixels.
-
-// The image is then saved as a new image.
-
-// The image is loaded and saved using the stb_image library.
-
-// The image is processed using CUDA.
 
 __global__ void removeRedEye(unsigned char* srcImage, unsigned char* dstImage, unsigned int width, unsigned int height, int channel)
 {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (x < width && y < height)
-    {
-        int red = srcImage[(y * width + x) * channel + 0];
-        int green = srcImage[(y * width + x) * channel + 1];
-        int blue = srcImage[(y * width + x) * channel + 2];
+    if (x < width && y < height) {
+        int idx = (y * width + x) * channel;
 
-        if (red > 150 && red > green && red > blue)
-        {
-            int sumRed = 0;
-            int sumGreen = 0;
-            int sumBlue = 0;
-            int count = 0;
+        unsigned char r = srcImage[idx];
+        unsigned char g = srcImage[idx + 1];
+        unsigned char b = srcImage[idx + 2];
 
-            for (int i = -2; i <= 2; i++)
-            {
-                for (int j = -2; j <= 2; j++)
-                {
-                    if (x + i >= 0 && x + i < width && y + j >= 0 && y + j < height)
-                    {
-                        sumRed += srcImage[((y + j) * width + (x + i)) * channel + 0];
-                        sumGreen += srcImage[((y + j) * width + (x + i)) * channel + 1];
-                        sumBlue += srcImage[((y + j) * width + (x + i)) * channel + 2];
-                        count++;
-                    }
-                }
-            }
+        if (r > 150 && g < 100 && b < 100) {
+			unsigned char r_avg = 0;
+			unsigned char g_avg = 0;
+			unsigned char b_avg = 0;
+			int count = 0;
 
+			for (int i = -1; i <= 1; i++) {
+				for (int j = -1; j <= 1; j++) {
+					int x_ = x + i;
+					int y_ = y + j;
 
-            dstImage[(y * width + x) * channel + 0] = sumRed / count;
-            dstImage[(y * width + x) * channel + 1] = sumGreen / count;
-            dstImage[(y * width + x) * channel + 2] = sumBlue / count;
-        }
-        else
-        {
-            dstImage[(y * width + x) * channel + 0] = red;
-            dstImage[(y * width + x) * channel + 1] = green;
-            dstImage[(y * width + x) * channel + 2] = blue;
-        }
+					if (x_ >= 0 && x_ < width && y_ >= 0 && y_ < height) {
+						int idx_ = (y_ * width + x_) * channel;
+						r_avg += srcImage[idx_];
+						g_avg += srcImage[idx_ + 1];
+						b_avg += srcImage[idx_ + 2];
+						count++;
+					}
+				}
+			}
+
+			r_avg /= count;
+			g_avg /= count;
+			b_avg /= count;
+
+			dstImage[idx] = r_avg;
+			dstImage[idx + 1] = g_avg;
+			dstImage[idx + 2] = b_avg;
+		}
+		else {
+			dstImage[idx] = r;
+			dstImage[idx + 1] = g;
+			dstImage[idx + 2] = b;
+		}
     }
 }
 
-
 int main()
 {
-	int width, height, channel;
-	unsigned char* srcImage = stbi_load("red_eye.jpg", &width, &height, &channel, 0);
+    int width, height, channel;
+    unsigned char* srcImage = stbi_load("red_eye.jpg", &width, &height, &channel, 0);
+    STBI_CHECK(srcImage != NULL);
 
-	unsigned char* dstImage = (unsigned char*)malloc(width * height * channel);
+    unsigned char* dstImage = (unsigned char*)malloc(width * height * channel);
+    if (dstImage == NULL) {
+        fprintf(stderr, "Failed to allocate memory for destination image\n");
+        exit(EXIT_FAILURE);
+    }
 
-	unsigned char* d_srcImage;
-	unsigned char* d_dstImage;
+    unsigned char* d_srcImage, * d_dstImage;
+    CUDA_CHECK(cudaMalloc(&d_srcImage, width * height * channel));
+    CUDA_CHECK(cudaMalloc(&d_dstImage, width * height * channel));
 
-	cudaMalloc(&d_srcImage, width * height * channel);
-	cudaMalloc(&d_dstImage, width * height * channel);
+    CUDA_CHECK(cudaMemcpy(d_srcImage, srcImage, width * height * channel, cudaMemcpyHostToDevice));
 
-	cudaMemcpy(d_srcImage, srcImage, width * height * channel, cudaMemcpyHostToDevice);
+    dim3 block(16, 16);
+    dim3 grid((width + block.x - 1) / block.x, (height + block.y - 1) / block.y);
 
-	dim3 block(32, 16);
-	dim3 grid((width + block.x - 1) / block.x, (height + block.y - 1) / block.y);
+    removeRedEye << <grid, block >> > (d_srcImage, d_dstImage, width, height, channel);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
 
-	removeRedEye << <grid, block >> > (d_srcImage, d_dstImage, width, height, channel);
+    CUDA_CHECK(cudaMemcpy(dstImage, d_dstImage, width * height * channel, cudaMemcpyDeviceToHost));
 
-	cudaMemcpy(dstImage, d_dstImage, width * height * channel, cudaMemcpyDeviceToHost);
+    stbi_write_jpg("red_eye_removed.jpg", width, height, channel, dstImage, 100);
 
-	stbi_write_jpg("red_eye_removed.jpg", width, height, channel, dstImage, 100);
+    stbi_image_free(srcImage);
+    free(dstImage);
 
-	stbi_image_free(srcImage);
-	free(dstImage);
+    CUDA_CHECK(cudaFree(d_srcImage));
+    CUDA_CHECK(cudaFree(d_dstImage));
 
-	cudaFree(d_srcImage);
-	cudaFree(d_dstImage);
-
-	return 0;
+    return 0;
 }
